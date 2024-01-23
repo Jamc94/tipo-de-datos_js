01e6f43e7c7fbd095e1d2b05072be47b057cd2ae
Joel Mondalgo <jmondalgoc@gmail.com>
2024-01-22 20:18:00 -0500

    añadiendo variables con diferentes tipos de datos

---
+++ script.js
@@ -0,0 +1,28 @@
+//TIPOS PRIMITIVOS
+
+//Number _ representa valores numéricos.
+let edad = 25;
+
+//String _ representa secuencia de caracteres.
+let nombre = "Joel";
+
+//Boolean _ representa un valor lógico verdadero o falso.
+let esMayorDeEdad = true;
+
+//Null _ representa la ausencia intencional de cualquier objeto o valor.
+let valorNulo = null;
+
+//Undefined _ representa la falta de definición de un valor.
+let variableNoDefinida;
+
+//Symbol _ introducido en ECMAScript 6, representa un identificador único e inmutable.
+let symbolInmutable = Symbol("descripción");
+
+
+
+console.log("Edad:", edad);
+console.log("Nombre:", nombre);
+console.log("Es mayor de edad:", esMayorDeEdad);
+console.log("Valor nulo:", valorNulo);
+console.log("Variable no definida:", variableNoDefinida);
+console.log("Símbolo:", symbolInmutable);
