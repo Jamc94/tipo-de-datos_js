3ce4f89705200f3b3941deebd99a393161c891eb
Joel Mondalgo <jmondalgoc@gmail.com>
2024-01-23 13:28:33 -0500

    se incluyeron situaciones de uso y consideraciones adicionales

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ let symbol2 = Symbol("descripción");
 //console.log(symbol1 === symbol2); //Salida: false
 
 
+//imprimir en la consola
 console.log("Edad:", edad);
 console.log("Precio:", precio);
 console.log("Distancia:", distancia);
@@ -52,3 +53,60 @@ console.log("Variable no definida:", variableNoDefinida);
 
 console.log("Símbolo:", symbol1);
 console.log("Símbolo:", symbol2);
+
+
+
+//Operaciones con Números
+let a = 10;
+let b = 5;
+let suma = a + b;
+let resta = a - b;
+let producto = a * b;
+let division = a / b;
+let modulo = a % b;
+
+console.log(`Suma ${a} + ${b} = ${suma}`);
+console.log(`Resta ${a} - ${b} = ${resta}`);
+console.log(`Multiplicación ${a} * ${b} = ${producto}`);
+console.log(`Division ${a} / ${b} = ${division}`);
+console.log(`modulo ${a} % ${b} = ${modulo}`);
+
+
+
+//Conversión entre Tipos
+let numeroComoCadena = String(42);
+let cadenaComoNumero = Number("3.14");
+let booleanComoNumero = Number(true);
+
+console.log(numeroComoCadena);
+console.log(cadenaComoNumero);
+console.log(booleanComoNumero);
+
+
+
+//Concatenación de Cadenas
+let hola = "hola";
+let despedida = "adios"
+let mensajeCompleto = hola + ", " + nombre + "! " + despedida;
+let mensajeCompleto2 = `${hola}, ${nombre}! ${despedida}`;
+
+console.log(mensajeCompleto);
+console.log(mensajeCompleto2);
+
+
+
+//Comparación de Valores
+let x = 5;
+let y = "5";
+let esIgual = x == y;
+let esIgualEstricto = x === y;
+
+console.log(esIgual);
+console.log(esIgualEstricto);
+
+
+
+// Uso de NaN (Not a Number)
+let resultadoInvalido = "a" / 2;
+
+console.log(resultadoInvalido);
